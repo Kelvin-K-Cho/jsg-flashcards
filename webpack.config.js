1dd5b74b0a5cc7b8567c4b4f3abaea67a1f174ce
Kelvin Cho <cho.k.kelvin@gmail.com>
2018-04-03 15:42:58 -0700

    Setup webpack

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+// var webpack = require('webpack');
+//
+// var plugins = [];
+// var devPlugins = [];
+//
+// var prodPlugins = [
+// 	new webpack.DefinePlugin({
+// 		'process.env': {
+// 			NODE_ENV: JSON.stringify('production')
+// 		}
+// 	}),
+// 	new webpack.optimize.UglifyJsPlugin({
+// 		compress: {
+// 			warnings: true
+// 		}
+// 	})
+// ];
+//
+// plugins = plugins.concat(
+// 	process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+// );
+
+module.exports = {
+	context: __dirname,
+	entry: './frontend/jsg-flashcards.jsx',
+	output: {
+		path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+		filename: 'bundle.js'
+	},
+	// plugins: plugins,
+	module: {
+		loaders: [
+			{
+				test: /\.jsx?$/,
+				exclude: /(node_modules|bower_components)/,
+				loader: 'babel-loader',
+				query: {
+					presets: ['env', 'react']
+				}
+			}
+		]
+	},
+	devtool: 'source-map',
+	resolve: {
+		extensions: ['.js', '.jsx', '*']
+	}
+};
