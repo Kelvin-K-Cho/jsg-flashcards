a77e006360b4d435b4c05a15d11c94a0afbfa236
Kelvin Cho <cho.k.kelvin@gmail.com>
2018-04-05 16:31:41 -0700

    Added comments for user auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,8 +12,6 @@
 
 class User < ApplicationRecord
 
-  # Validations
-
   # Ensures that the instance of a user cannot exist unless the user has a unique username.
   validates :username, presence: true, uniqueness: true
 
@@ -27,31 +25,60 @@ class User < ApplicationRecord
   # Define an instance variable to hold the password.
   attr_reader :password
 
+  # After the user object has been instantiated, it will run ensure session_token.
+  # This will make sure that there's always a session_token available for the logged in user.
   after_initialize :ensure_session_token
 
+  # Function to lookup the credentials of the entered username and password.
   def self.find_by_credentials(username, password)
+
+    # Create a temporary variable and set it to the user by looking up the username.
     user = User.find_by(username: username)
+
+    # If the user exists and the entered password is correct,
+    # return the user.  Otherwise, return nil.
     user && user.is_password?(password) ? user : nil
+
   end
 
-
+  # Writer function for storing the password.
   def password=(password)
+
+    # Store the password in the instance variable.
     @password = password
+
+    # Set the password_digest to the hashed version of the password.
     self.password_digest = BCrypt::Password.create(password)
+
   end
 
+  # Function to check if the entered password matches the stored password.
   def is_password?(password)
+
+    #Turn the password_digest into the password and check against the password.
     BCrypt::Password.new(self.password_digest).is_password?(password)
+
   end
 
+  # Function to check the session token.
   def ensure_session_token
+
+    # If the session token already exists, return that session_token.
+    # Otherwise, create a new random session_token to verify across the browser.
     self.session_token ||= SecureRandom.urlsafe_base64
+
   end
 
+  # Function to change the session_token.
   def reset_session_token!
+
+    # Change the session token to a new one.
     self.session_token = SecureRandom.urlsafe_base64
+    # Store the new session_token in the database.
     self.save!
+    # Return the new session_token.
     self.session_token
+
   end
 
 end
