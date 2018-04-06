3ca7e9d10d11f63832669c80d186cc0f72f4073a
Kelvin Cho <cho.k.kelvin@gmail.com>
2018-04-05 21:03:46 -0700

    Fixed views and wrote out the store file.

---
+++ frontend/store/store.js
@@ -0,0 +1,15 @@
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from '../reducers/root';
+
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV !== 'production') {
+	const { logger } = require('redux-logger');
+	middlewares.push(logger);
+}
+
+const configureStore = (preloadedState = {}) =>
+	createStore(rootReducer, preloadedState, applyMiddleware(...middlewares));
+
+export default configureStore;
