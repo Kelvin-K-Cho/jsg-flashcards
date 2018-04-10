380466ed7d3c81ad5035787d285dcb74b0509537
Kelvin Cho <cho.k.kelvin@gmail.com>
2018-04-09 22:59:32 -0700

    Fixed root file

--- frontend/reducers/root.js
+++ frontend/reducers/root.js
@@ -1,6 +1,5 @@
 import { combineReducers } from 'redux';
 import sessionReducer from './session';
-import errorsReducer from './errors';
 import entitiesReducer from './entities';
 
 const RootReducer = combineReducers({
