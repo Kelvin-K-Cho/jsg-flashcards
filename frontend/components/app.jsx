55b6c1f4bd9db31a9c191045d68695b94ceb04a6
Kelvin Cho <cho.k.kelvin@gmail.com>
2018-04-10 15:43:57 -0700

    Added NavBar

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -3,14 +3,14 @@ import { Provider } from 'react-redux';
 import { Route, Redirect, Switch, Link, HashRouter } from 'react-router-dom';
 import SignupContainer from './session/signup_container';
 import LoginContainer from './session/login_container';
-import NavBarContainer from './navbar/navbar_container';
+import NavbarContainer from './navbar/navbar_container';
 import { AuthRoute, ProtectedRoute } from '../utils/route';
 import Home from './home/home';
 
 const App = () => (
 	<div>
 		<header>
-			<Route path="/" component={NavBarContainer} />
+			<Route path="/" component={NavbarContainer} />
 		</header>
 		<Switch>
 			<Route exact path="/" component={Home} />
