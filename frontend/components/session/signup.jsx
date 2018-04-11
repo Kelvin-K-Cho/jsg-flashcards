30f9d24d1b1ecfdf1e6522eaf32967dbd5555ce6
Kelvin Cho <cho.k.kelvin@gmail.com>
2018-04-11 11:06:55 -0700

    Completed writing session containers

---
+++ frontend/components/session/signup.jsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+class Signup extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			username: '',
+			password: ''
+		};
+		this.handleSubmit = this.handleSubmit.bind(this);
+		this.handleDemo = this.handleDemo.bind(this);
+	}
+
+	handleInput(type) {
+		return event => {
+			this.setState({
+				[type]: event.target.value
+			});
+		};
+	}
+
+	handleSubmit(event) {
+		event.preventDefault();
+		this.props
+			.createNewUser(this.state)
+			.then(() => this.props.history.push(`/study`));
+	}
+
+	handleDemo(event) {
+		event.preventDefault();
+		const demo = { username: 'demo', password: 'secret' };
+		this.props.login(demo).then(() => this.props.history.push('/study'));
+	}
+
+	render() {
+		return (
+			<div className="div-session">
+				<h1>Hit the ground running.</h1>
+				<h2>Sign up for free</h2>
+				<form className="form-session">
+					<label id="username-session">
+						<input
+							type="text"
+							value={this.state.username}
+							onChange={this.handleInput('username')}
+							placeholder="Username"
+						/>
+					</label>
+					<label id="password-session">
+						<input
+							type="password"
+							value={this.state.password}
+							onChange={this.handleInput('password')}
+							placeholder="Password"
+						/>
+					</label>
+					<button onClick={this.handleSubmit}>Sign Up</button>
+					<button id="button-demo" onClick={this.handleDemo}>
+						Demo User
+					</button>
+				</form>
+				<div id="check-account">
+					Already have an account? &nbsp;<Link id="link-session" to="/login">
+						Log In
+					</Link>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default Signup;
