acacf1a8f2d43f61130e50a8bb4f41b8748c41a2
Kelvin Cho <cho.k.kelvin@gmail.com>
2018-04-12 09:58:20 -0700

    Fixed Home component

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 
-const Home = () => {
+const Home = props => {
 	const body = (
 		<div>
 			<h1>Job Search Guide Flashcards</h1>
 			<h2>Want to hit the ground running?</h2>
-			<h3>
+			<h2>
 				Study these flashcards and show employers you're more than just about
 				the culture and fit.
-			</h3>
-			<h4>YOU ARE A SOFTWARE DEVELOPER!</h4>
+			</h2>
+			<h2>YOU ARE A SOFTWARE DEVELOPER!</h2>
 		</div>
 	);
-	return { body };
+	return body;
 };
 
 export default Home;
