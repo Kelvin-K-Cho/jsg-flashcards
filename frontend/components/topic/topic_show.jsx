d71515d1070ec847c1fc7969d8ba22253c8f9219
Kelvin Cho <cho.k.kelvin@gmail.com>
2018-04-16 19:52:04 -0700

    Finish topic shows

--- frontend/components/topic/topic_show.jsx
+++ frontend/components/topic/topic_show.jsx
@@ -18,7 +18,15 @@ class TopicShow extends React.Component {
 		if (!topic) {
 			return <Loader />;
 		} else {
-			return <div className="topic-title">{topic.title}</div>;
+			let title = <div className="topic-title">{topic.title}</div>;
+			let text = <div className="studyset-header">List of Study Sets:</div>;
+			let list = <ul className="cards-list">placeholder</ul>;
+			return (
+				<div>
+					{title}
+					{text}
+				</div>
+			);
 		}
 	}
 }
