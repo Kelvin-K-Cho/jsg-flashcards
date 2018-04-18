a449dcf43dfaaadbe6cb796434f069a16011662e
Kelvin Cho <cho.k.kelvin@gmail.com>
2018-04-17 21:57:04 -0700

    Removed subject index container

--- frontend/components/topic/topic_show.jsx
+++ frontend/components/topic/topic_show.jsx
@@ -5,30 +5,35 @@ import SubjectIndexByTopic from '../subject/subject_index_by_topic';
 
 class TopicShow extends React.Component {
 	componentDidMount() {
-		this.props.fetchTopic(this.props.match.params.topicId);
+		this.props
+			.fetchTopic(this.props.match.params.topicId)
+			.then(this.props.fetchSubjectsByTopic(this.props.match.params.topicId));
 	}
 
 	componentWillReceiveProps(nextProps) {
 		if (this.props.match.params.topicId !== nextProps.match.params.topicId) {
-			this.props.fetchTopic(nextProps.match.params.topicId);
+			this.props
+				.fetchTopic(nextProps.match.params.topicId)
+				.then(this.props.fetchSubjectsByTopic(this.props.match.params.topicId));
 		}
 	}
 
 	render() {
-		const { topic } = this.props;
+		const { topic, subjects } = this.props;
 		if (!topic) {
 			return <Loader />;
 		} else {
 			let title = <div className="topic-title">{topic.title}</div>;
 			let text = <div className="studyset-header">List of Study Sets:</div>;
-			let list = <SubjectIndexByTopic />;
+			let list = (
+				<SubjectIndexByTopic key={topic.id} topic={topic} topicId={topic.id}>
+					Placeholder
+				</SubjectIndexByTopic>
+			);
 			return (
 				<div>
 					{title}
 					{text}
-					<SubjectIndexByTopic key={topic.id} topic={topic} topicId={topic.id}>
-						Placeholder
-					</SubjectIndexByTopic>
 				</div>
 			);
 		}
--- frontend/components/topic/topic_show_container.js
+++ frontend/components/topic/topic_show_container.js
@@ -1,13 +1,17 @@
 import { connect } from 'react-redux';
 import TopicShow from './topic_show';
 import { fetchTopic } from '../../actions/topics';
+import { fetchSubjectsByTopic } from '../../actions/subjects';
+import { selectSubjects } from '../../reducers/selectors';
 
 const mapStateToProps = (state, ownProps) => ({
-	topic: state.entities.topics[ownProps.match.params.topicId]
+	topic: state.entities.topics[ownProps.match.params.topicId],
+	subjects: selectSubjects(state)
 });
 
 const mapDispatchToProps = dispatch => ({
-	fetchTopic: topicId => dispatch(fetchTopic(topicId))
+	fetchTopic: topicId => dispatch(fetchTopic(topicId)),
+	fetchSubjectsByTopic: topicId => dispatch(fetchSubjectsByTopic(topicId))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(TopicShow);
