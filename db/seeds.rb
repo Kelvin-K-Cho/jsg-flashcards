052c082e70369ad29a56fdeb0eb26ff9a3aedc62
Kelvin Cho <cho.k.kelvin@gmail.com>
2018-04-18 12:30:46 -0700

    Updated seeds table

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,49 +6,34 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+#=========
+# Deletes
+#=========
+
 User.destroy_all
 Topic.destroy_all
 Subject.destroy_all
+Question.destroy_all
+Answer.destroy_all
+
+#===========
+# Demo User
+#===========
 
 user = User.create(username: "demo", password: "secret")
 
-algorithm = Topic.create(
+#========
+# Topics
+#========
+
+algorithms = Topic.create(
   title: "Algorithms",
   image_url: "https://s3-us-west-1.amazonaws.com/hallyu-dev/images/jsg/Algorithms.jpg"
 )
 
-algorithm.save!
-
-heap = Subject.create(
-  title: "Heap",
-  notes: "A heap is a data structure that is a complete binary tree and it satisfies the heap property. The heap property (in a max heap) is when the value of parent node is greater than or equal to its children and (in a min heap) the value of the parent is less than or equal to its children. Except the root node, the node at the 'top' of the heap, which has no parents.",
-  topic_id: algorithm.id
-)
-
-recursion = Subject.create(
-  title: "Recursion",
-  notes: "The process in which a function calls itself directly or indirectly is called recursion and the corresponding function is called as recursive function.",
-  topic_id: algorithm.id
-)
-
-worldwideweb = Topic.create(
+world_wide_web = Topic.create(
   title: "World Wide Web",
   image_url: "https://s3-us-west-1.amazonaws.com/hallyu-dev/images/jsg/Web.jpg"
-)
-
-worldwideweb.save!
-
-
-dns = Subject.create(
-  title: "DNS",
-  notes: "DNS stands for Domain Name Servers or Domain Name System. It acts as the backbone or phonebook of the internet.",
-  topic_id: worldwideweb.id
-)
-
-http = Subject.create(
-  title: "HTTP",
-  notes: "HTTP stands for HyperText Transfer Protocol and it's the basis for sending data across the World Wide Web.",
-  topic_id: worldwideweb.id
 )
 
 javascript = Topic.create(
@@ -56,7 +41,62 @@ javascript = Topic.create(
   image_url: "https://s3-us-west-1.amazonaws.com/hallyu-dev/images/jsg/JavaScript.png"
 )
 
+
+miscellaneous = Topic.create(
+  title: "Miscellaneous",
+  image_url: "https://s3-us-west-1.amazonaws.com/hallyu-dev/images/jsg/MiscBox.png"
+)
+
+algorithms.save!
+world_wide_web.save!
 javascript.save!
+miscellaneous.save!
+
+#==========
+# Subjects
+#==========
+
+#------------
+# Algorithms
+#------------
+
+heap = Subject.create(
+  title: "Heap",
+  notes: "A heap is a data structure that is a complete binary tree and it satisfies the heap property. The heap property (in a max heap) is when the value of parent node is greater than or equal to its children and (in a min heap) the value of the parent is less than or equal to its children. Except the root node, the node at the 'top' of the heap, which has no parents.",
+  topic_id: algorithms.id
+)
+
+recursion = Subject.create(
+  title: "Recursion",
+  notes: "The process in which a function calls itself directly or indirectly is called recursion and the corresponding function is called as recursive function.",
+  topic_id: algorithms.id
+)
+
+heap.save!
+recursion.save!
+
+#----------------
+# World Wide Web
+#----------------
+
+dns = Subject.create(
+  title: "DNS",
+  notes: "DNS stands for Domain Name Servers or Domain Name System. It acts as the backbone or phonebook of the internet.",
+  topic_id: world_wide_web.id
+)
+
+http = Subject.create(
+  title: "HTTP",
+  notes: "HTTP stands for HyperText Transfer Protocol and it's the basis for sending data across the World Wide Web.",
+  topic_id: world_wide_web.id
+)
+
+dns.save!
+http.save!
+
+#------------
+# JavaScript
+#------------
 
 new_keyword = Subject.create(
   title: "new",
@@ -70,12 +110,12 @@ event_loop = Subject.create(
   topic_id: javascript.id
 )
 
-miscellaneous = Topic.create(
-  title: "Miscellaneous",
-  image_url: "https://s3-us-west-1.amazonaws.com/hallyu-dev/images/jsg/MiscBox.png"
-)
+new_keyword.save!
+event_loop.save!
 
-miscellaneous.save!
+#---------------
+# Miscellaneous
+#---------------
 
 grid = Subject.create(
   title: "grid",
@@ -88,3 +128,140 @@ redux = Subject.create(
   notes: "Redux is a library that allows React application to have state. This state is represent as a giant javascript object (single immutable state tree)",
   topic_id: miscellaneous.id
 )
+
+grid.save!
+redux.save!
+
+#===========
+# Questions
+#===========
+
+heap_why_q = Question.create(
+  sentence: "What real life problem does implementing a heap solve?",
+  subject_id: heap.id
+)
+
+heap_runtime_max_q = Question.create(
+  sentence: "What is the runtime of finding the max/min of a heap?",
+  subject_id: heap.id
+)
+
+recursion_base_case_q = Question.create(
+  sentence: "What is a base case?",
+  subject_id: heap.id
+)
+
+dns_name_q = Question.create(
+  sentence: "What is the full form of DNS?",
+  subject_id: dns.id
+)
+
+http_case_sensitive_q = Question.create(
+  sentence: "Are HTTP methods case-sensitive?",
+  subject_id: http.id
+)
+
+new_keyword_kind_q = Question.create(
+  sentence: "What kind of function is 'new'?",
+  subject_id: new_keyword.id
+)
+
+new_keyword_invoke_q = Question.create(
+  sentence: "What happens when you try to create a new object without the new keyword?",
+  subject_id: new_keyword.id
+)
+
+event_loop_components_q = Question.create(
+  sentence: "What are the components that make up the event loop cycle?",
+  subject_id: event_loop.id
+)
+
+grid_why_q = Question.create(
+  sentence: "Why do people use grids to style websites?",
+  subject_id: grid.id
+)
+
+redux_store_methods_q = Question.create(
+  sentence: "What are the different redux store methods?",
+  subject_id: redux.id
+)
+
+redux_context_q = Question.create(
+  sentence: "How can you make the redux store available to all components?",
+  subject_id: redux.id
+)
+
+heap_why_q.save!
+heap_runtime_max_q.save!
+recursion_base_case_q.save!
+dns_name_q.save!
+http_case_sensitive_q.save!
+new_keyword_kind_q.save!
+new_keyword_invoke_q.save!
+event_loop_components_q.save!
+grid_why_q.save!
+redux_store_methods_q.save!
+redux_context_q.save!
+
+#=========
+# Answers
+#=========
+
+heap_why_a1 = Answer.create(
+  sentence: "Priority Queue",
+  correct: true,
+  question_id: heap_why_q.id
+)
+
+heap_why_a2 = Answer.create(
+  sentence: "Social Network",
+  correct: false,
+  question_id: heap_why_q.id
+)
+
+heap_runtime_max_a1 = Answer.create(
+  sentence: "O(logN)",
+  correct: false,
+  question_id: heap_why_q.id
+)
+
+heap_runtime_max_a2 = Answer.create(
+  sentence: "O(NlogN)",
+  correct: false,
+  question_id: heap_why_q.id
+)
+
+heap_runtime_max_a3 = Answer.create(
+  sentence: "O(1)",
+  correct: true,
+  question_id: heap_why_q.id
+)
+
+heap_runtime_max_a4 = Answer.create(
+  sentence: "O(N)",
+  correct: false,
+  question_id: heap_why_q.id
+)
+
+recursion_base_case_a1 = Answer.create(
+  sentence: "The default return value of a switch statement in the recursive function.",
+  correct: false,
+  question_id: recursion_base_case_q.id
+)
+
+recursion_base_case_a2 = Answer.create(
+  sentence: "The solution to the smallest input of the function",
+  correct: true,
+  question_id: recursion_base_case_q.id
+)
+
+
+
+heap_why_a1.save!
+heap_why_a2.save!
+heap_runtime_max_a1.save!
+heap_runtime_max_a2.save!
+heap_runtime_max_a3.save!
+heap_runtime_max_a4.save!
+recursion_base_case_a1.save!
+recursion_base_case_a2.save!
