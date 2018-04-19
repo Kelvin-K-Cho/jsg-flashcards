14fd18f51e04f86e6e39f4e36ae4e303b53b0e33
Kelvin Cho <cho.k.kelvin@gmail.com>
2018-04-18 20:49:50 -0700

    Updated subject show container with questions and answers

--- frontend/components/subject/subject_show_container.js
+++ frontend/components/subject/subject_show_container.js
@@ -1,13 +1,22 @@
 import { connect } from 'react-redux';
 import SubjectShow from './subject_show';
 import { fetchSubject } from '../../actions/subjects';
+import { fetchQuestionsBySubject } from '../../actions/questions';
+import { fetchAnswersByQuestion } from '../../actions/answers';
+import { selectQuestions, selectAnswers } from '../../reducers/selectors';
 
 const mapStateToProps = (state, ownProps) => ({
-	subject: state.entities.subjects[ownProps.match.params.subjectId]
+	subject: state.entities.subjects[ownProps.match.params.subjectId],
+	questions: selectQuestions(state),
+	answers: selectAnswers(state)
 });
 
 const mapDispatchToProps = dispatch => ({
-	fetchSubject: subjectId => dispatch(fetchSubject(subjectId))
+	fetchSubject: subjectId => dispatch(fetchSubject(subjectId)),
+	fetchQuestionsBySubject: subjectId =>
+		dispatch(fetchQuestionsBySubject(subjectId)),
+	fetchAnswersByQuestion: questionId =>
+		dispatch(fetchAnswersByQuestion(questionId))
 });
 
 const SubjectShowContainer = connect(mapStateToProps, mapDispatchToProps)(
--- frontend/reducers/entities.js
+++ frontend/reducers/entities.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import TopicsReducer from './topics';
 import SubjectsReducer from './subjects';
 import QuestionsReducer from './questions';
+import AnswersReducer from './answers';
 
 const EntitiesReducer = combineReducers({
 	topics: TopicsReducer,
 	subjects: SubjectsReducer,
-	questions: QuestionsReducer
+	questions: QuestionsReducer,
+	answers: AnswersReducer
 });
 
 export default EntitiesReducer;
