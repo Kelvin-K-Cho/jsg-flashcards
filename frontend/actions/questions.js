9b6cbad9fed2f792c70c9e824f3ec73582a18fe0
Kelvin Cho <cho.k.kelvin@gmail.com>
2018-04-18 19:30:29 -0700

    Finish writing question action creator

--- frontend/actions/questions.js
+++ frontend/actions/questions.js
@@ -17,3 +17,27 @@ const fetchQuestions = () => dispatch =>
 	QuestionAPIUtil.fetchQuestions().then(questions => {
 		dispatch(receiveAllQuestions(questions));
 	});
+
+const fetchQuestion = id => dispatch =>
+	QuestionAPIUtil.fetchQuestion(id).then(question => {
+		dispatch(receiveQuestion(question[id]));
+	});
+
+const fetchQuestionsBySubject = (subjectId, id) => dispatch =>
+	QuestionAPIUtil.fetchQuestionsBySubject(subjectId, id).then(questions => {
+		dispatch(receiveAllQuestions(questions));
+	});
+
+const fetchQuestionBySubject = (subjectId, id) => dispatch =>
+	QuestionAPIUtil.fetchQuestionBySubject(subjectId, id).then(question => {
+		dispatch(receiveQuestion(question[id]));
+	});
+
+export {
+	RECEIVE_ALL_QUESTIONS,
+	RECEIVE_QUESTION,
+	fetchQuestions,
+	fetchQuestion,
+	fetchQuestionsBySubject,
+	fetchQuestionBySubject
+};
