4564f6ef230547c192a47a398ea5e7571f01c6d2
Kelvin Cho <cho.k.kelvin@gmail.com>
2018-04-18 21:00:08 -0700

    Update answers model

--- app/controllers/api/answers_controller.rb
+++ app/controllers/api/answers_controller.rb
@@ -1,7 +1,9 @@
 class Api::AnswersController < ApplicationController
 
   def index
-    if params[:question_id]
+    if params[:subject_id]
+      @answers = Answer.all.includes(:subject)
+    elsif params[:question_id]
       @answers = Answer.where('question_id = ?', params[:question_id]).order(id: :asc)
     else
       @answers = Answer.all
