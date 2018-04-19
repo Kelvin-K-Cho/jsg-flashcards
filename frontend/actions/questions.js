03169b570dd6e9a4d45c3afa0aea46199c2c5dd2
Kelvin Cho <cho.k.kelvin@gmail.com>
2018-04-18 17:06:35 -0700

    Started writing questions action creator

---
+++ frontend/actions/questions.js
@@ -0,0 +1,14 @@
+import * as QuestionAPIUtil from '../utils/questions';
+
+const RECEIVE_ALL_QUESTIONS = 'RECEIVE_ALL_QUESTIONS';
+const RECEIVE_QUESTION = 'RECEIVE_QUESTION';
+
+const receiveAllQuestions = questions => ({
+	type: RECEIVE_ALL_QUESTIONS,
+	questions
+});
+
+const receiveQuestion = question => ({
+	type: RECEIVE_QUESTION,
+	question
+});
