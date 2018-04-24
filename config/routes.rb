c020647f7e1c45a24c2ac34b965ed3b5ad94a913
Kelvin Cho <cho.k.kelvin@gmail.com>
2018-04-24 15:25:24 -0700

    Added routes for search

--- config/routes.rb
+++ config/routes.rb
@@ -2,22 +2,31 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :api, defaults: { format: :json } do
+
     resources :users, only: [:create, :show]
+
     resource :session, only: [:create, :destroy]
+
     resources :topics, only: [:index, :show] do
       resources :subjects, only: [:index, :show]
     end
+
     resources :subjects, only: [:index, :show] do
       resources :questions, only: [:index, :show]
       resources :answers, only: [:index, :show]
     end
+
     resources :questions, only: [:index, :show] do
       resources :answers, only: [:index, :show]
     end
+
     resources :answers, only: [:index, :show]
+
     resources :weeks, only: [:index, :show] do
       resources :days, only: [:index, :show]
     end
+
+    resources :searches, only: [:index]
   end
 
   root "static_pages#root"
