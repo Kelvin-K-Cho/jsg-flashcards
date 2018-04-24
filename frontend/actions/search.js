00ba22112e74a9e072b6ba14d0cabad20759e4e5
Kelvin Cho <cho.k.kelvin@gmail.com>
2018-04-24 15:44:55 -0700

    Started search results action creator

---
+++ frontend/actions/search.js
@@ -0,0 +1,11 @@
+import search from '../utils/search';
+// import { receiveAllSubjects } from './subjects';
+//
+// const searchSubjects = value => dispatch =>
+// 	search(value).then(subjects => {
+// 		dispatch(receiveAllSubjects(subjects));
+// 	});
+//
+// export { searchSubjects };
+
+//refactor to results;
