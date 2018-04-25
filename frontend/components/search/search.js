7573a6f6ff978af5b17c3e403712d75b9ee4bd4e
Kelvin Cho <cho.k.kelvin@gmail.com>
2018-04-25 15:28:07 -0700

    Finished building Search Component

--- frontend/components/search/search.js
+++ frontend/components/search/search.js
@@ -1,11 +1,48 @@
 import React from 'react';
+import { Form, FormGroup, Input, FormText } from 'reactstrap';
 
 class Search extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			value: '',
-			results: {}
+			value: ''
+		};
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	handleInput(type) {
+		return e => {
+			this.setState({
+				[type]: e.target.value
+			});
 		};
 	}
+
+	handleSubmit(e) {
+		e.preventDeafult();
+		this.props
+			.fetchResults(this.state)
+			.then(value => this.props.history.push(`/search?value=${value}`));
+	}
+
+	render() {
+		return (
+			<Form>
+				<FormGroup>
+					<Input
+						type="text"
+						name="search"
+						id="search"
+						value={this.state.value}
+						onChange={this.handleInput('value')}
+						placeholder="Search By Subject"
+					/>
+				</FormGroup>
+
+				<Button onClick={this.handleSubmit} />
+			</Form>
+		);
+	}
 }
+
+export default Search;
